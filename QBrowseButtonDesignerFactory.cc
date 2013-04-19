eecaae6ed3760dfb372ec0a8005dc142daaa544a
Peter Hille (png!das-system) <peter@das-system-networks.de>
2013-04-19 03:46:43 +0200

    add Doxygen comments for Qt Designer plugin.

--- QBrowseButtonDesignerFactory.cc
+++ QBrowseButtonDesignerFactory.cc
@@ -1,3 +1,14 @@
+/** \file
+ *
+ * \brief Implementation of \a QBrowseButtonDesignerFactory
+ *
+ * This file provides the implementation of the Qt Designer plugin for
+ * \a QBrowseButton.
+ *
+ * \author Peter 'png' Hille <peter@das-system-networks>
+ *
+ * \version 1.0
+ */
 #include <QDebug>
 #include <QMessageBox>
 #include <QtPlugin>
@@ -6,12 +17,26 @@
 #include "QBrowseButton.h"
 #include "QBrowseButtonDesignerFactory.h"
 
+
+/** \brief Default constructor
+ *
+ * Initializes the QBrowseButton factory for Qt Designer.
+ */
 QBrowseButtonDesignerFactory::QBrowseButtonDesignerFactory(QDeclarativeItem *parent) :
     QObject(parent)
 {
+    // ... nothing ...
 }
 
 
+/** \brief Create QBrowseButton instance
+ *
+ * Creates a new QBrowseButton widget for use in Qt Designer.
+ *
+ * \param parent Parent QWidget of the newly created QBrowseButton
+ *
+ * \return Pointer to newly created QBrowseButton instance
+ */
 QWidget* QBrowseButtonDesignerFactory::createWidget(QWidget *parent) {
     QBrowseButton* widget = nullptr;
     try {
@@ -30,36 +55,64 @@ QWidget* QBrowseButtonDesignerFactory::createWidget(QWidget *parent) {
 }
 
 
+/** \brief Widget group name
+ *
+ * \return Name of the group under which our widget shall show up in Qt Designer
+ */
 QString QBrowseButtonDesignerFactory::group() const {
-    return "#das-system Networks";
+    return "#das-system";
 }
 
 
+/** \brief Widget icon
+ *
+ * \return Icon to show in Qt Designer's widget list
+ */
 QIcon QBrowseButtonDesignerFactory::icon() const {
     return QIcon::fromTheme("folder-open");
 }
 
 
+/** \brief Header name for Qt Designer
+ *
+ * \return Name of C++ header to include for our widget
+ */
 QString QBrowseButtonDesignerFactory::includeFile() const {
     return "QBrowseButton.h";
 }
 
 
+/** \brief Tooltip text
+ *
+ * \return Tooltip text that is shown for our widget
+ */
 QString QBrowseButtonDesignerFactory::toolTip() const {
     return tr("File/Directory Browse Button");
 }
 
 
+/** \brief "What's this?" description
+ *
+ * \return Descriptive text for "What's This?"
+ */
 QString QBrowseButtonDesignerFactory::whatsThis() const {
     return tr("Button to browse for directories or files.");
 }
 
 
+/** \brief Is our widget a container?
+ *
+ * \return Boolean flag indicating wether our widget is a container (false)
+ */
 bool QBrowseButtonDesignerFactory::isContainer() const {
     return false;
 }
 
 
+/** \brief Widget name
+ *
+ * \return Name of our widget to show in Qt Designer
+ */
 QString QBrowseButtonDesignerFactory::name() const {
     return "QBrowseButton";
 }
--- QBrowseButtonDesignerFactory.h
+++ QBrowseButtonDesignerFactory.h
@@ -1,3 +1,14 @@
+/** \file
+ *
+ * \brief Declaration of \a QBrowseButtonDesignerFactory
+ *
+ * This file provides the prototypes of the Qt Designer plugin for
+ * \a QBrowseButton.
+ *
+ * \author Peter 'png' Hille <peter@das-system-networks>
+ *
+ * \version 1.0
+ */
 #ifndef QBROWSEBUTTONDESIGNERFACTORY_H
 #define QBROWSEBUTTONDESIGNERFACTORY_H
 
@@ -5,6 +16,16 @@
 #include <QtDeclarative/QDeclarativeItem>
 #include <QtDesigner/QDesignerCustomWidgetInterface>
 
+
+/** \brief Qt Designer plugin for \a QBrowseButton
+ *
+ * This class provides a Qt Designer plugin which enables the \a QBrowseButton
+ * class to be used for UI design.
+ *
+ * \author Peter 'png' Hille <peter@das-system-networks.de>
+ *
+ * \version 1.0 Initial implementation
+ */
 class QBrowseButtonDesignerFactory : public QObject,
         public QDesignerCustomWidgetInterface
 {

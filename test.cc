0f0b278eee55970111a63124b064b025750970f6
Peter Hille (png!das-system) <peter@das-system-networks.de>
2013-04-19 08:00:01 +0200

    add simple testing executable.

---
+++ test.cc
@@ -0,0 +1,12 @@
+#include <QApplication>
+
+#include "QBrowseButton.h"
+
+int main(int argc, char** argv) {
+    QApplication a(argc, argv);
+
+    QBrowseButton* b = new QBrowseButton();
+    b->show();
+
+    return a.exec();
+}
